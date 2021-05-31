8fe45c1ee6a2afcce444fb11f51f2b866221be68
Navin Shrinivas <karupal2002@gmail.com>
2021-05-31 16:44:16 +0530

    high experimantal code -2
    Signed-off-by: Navin Shrinivas <karupal2002@gmail.com>
    
    this code has chances to make it to main branch
    But as of now this code renders your pc unusable unless u
    force shutdowm and start again.

--- FLOPanalyzer.c
+++ FLOPanalyzer.c
@@ -1,36 +1,117 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 #include<pthread.h>
-#include <unistd.h>
+#include<unistd.h>
 typedef long long int ll;
 
-struct test{
-    ll ops;
-    ll thread;
+//structure of the struct so that the function
+//can understand derefrencing
+struct result{
+    int thread_id;
+    ll fadd;
+    ll fsub;
+    ll fdiv;
+    ll fmul;
 };
-void *testfunc(void* struc)//just to test the multithreading operations
-{
-    ((struct test*)struc)->ops=0;
-    while(1)
+void *flopcalculator(void* threadprops){
+    /*registers are memory locations in cpu,
+    here we have used register so as to not loose
+    anyperformace due to memory latency*/
+    register double v1=0.435e-308; 
+    register double v2=0.45e308; 
+    register double v3=0.234e-308; 
+    register double v4=0.453e308; 
+    register double v5=0.423e-308; 
+    //this spam of 64bit double standard floating point numbers
+    //is to make sure that compiler doesnt take any advantage of
+    //operation specific pipelines that speeds up the process that 
+    //is present in CPU pipelines
+
+    while(true)
     {
-        double a=0.2324234+0.2353465465+0.2423+7.34543+7.23423;
-        ((struct test*)struc)->ops+=5;
+        double* chc_add=(double*)malloc(sizeof(double));
+        double* chc_sub=(double*)malloc(sizeof(double));
+        double* chc_div=(double*)malloc(sizeof(double));
+        double* chc_mul=(double*)malloc(sizeof(double));
+        if(((struct result*)threadprops)->fadd==0)
+        {
+            *chc_add=v1+v2+v3+v4+v5; //few x86 architechture have a optimised pipline for fadd
+            ((struct result*)threadprops)->fadd+=10;
+        }
+        if(((struct result*)threadprops)->fsub==0)
+        {
+            *chc_sub=v1-v2-v3-v4-v5;
+            ((struct result*)threadprops)->fsub+=10;
+        }
+        if(((struct result*)threadprops)->fmul==0)
+        {
+            *chc_mul=v1*v2*v3*v4*v5; //few x86 architechture have a optimised pipline for fmul
+            ((struct result*)threadprops)->fmul+=10;
+        }
+        if(((struct result*)threadprops)->fdiv==0)
+        {
+            *chc_div=(v1/v2)+(v3/v4)+(v5);
+            ((struct result*)threadprops)->fdiv+=10;
+        }
     }
 }
-ll main(){
-    ll tot;
+int main(){
+    int tot;
     printf("Total number of threads to run on :");
     scanf("%i",&tot);
+    int time;
+    printf("Total time for test :");
+    scanf("%i",&time);
     pthread_t threads[tot];
-    struct test test1[tot];
+    struct result results[tot];
     for(ll i=0;i<tot;i++)
     {
-        ((struct test*)test1+i)->thread=i;
-        pthread_create(&threads[i],NULL,testfunc,test1+i);
+        ((struct result*)results+i)->thread_id=i;
+        ((struct result*)results+i)->fadd=-1;
+        ((struct result*)results+i)->fsub=-1;
+        ((struct result*)results+i)->fmul=-1;
+        ((struct result*)results+i)->fdiv=-1;  
     }
-    sleep(20);
-    for(ll i=0;i<tot;i++){
-        printf("%ist threads ops value : %i\n",((struct test*)test1+i)->thread,((struct test*)test1+i)->ops);
+    for(ll i=0;i<tot;i++)
+    {   
+        printf("Starting fadd module!");
+        ((struct result*)results+i)->fadd=0;
+        pthread_create(&threads[i],NULL,flopcalculator,results+i);
+    }
+    sleep(time/4);
+    for(int i=0;i<tot;i++){
         pthread_cancel(threads[i]);
     }
+    for(ll i=0;i<tot;i++)
+    {   
+        printf("Starting fsub module!");
+        ((struct result*)results+i)->fsub=0;
+        pthread_create(&threads[i],NULL,flopcalculator,results+i);
     }
-        
+    sleep(time/4);
+    for(int i=0;i<tot;i++){
+        pthread_cancel(threads[i]);
+    }
+    for(ll i=0;i<tot;i++)
+    {   
+        printf("Starting fmul module!");
+        ((struct result*)results+i)->fmul=0;
+        pthread_create(&threads[i],NULL,flopcalculator,results+i);
+    }
+    sleep(time/4);
+    for(int i=0;i<tot;i++){
+        pthread_cancel(threads[i]);
+    }
+    for(ll i=0;i<tot;i++)
+    {   
+        printf("Starting fdiv module!");
+        ((struct result*)results+i)->fdiv=0;
+        pthread_create(&threads[i],NULL,flopcalculator,results+i);
+    }
+    sleep(time/4);
+    for(int i=0;i<tot;i++){
+        pthread_cancel(threads[i]);
+    }
+
+}        
