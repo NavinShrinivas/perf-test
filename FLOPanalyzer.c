c3997ae174c825444e3520f0b811e24b3c82b8f2
Navin Shrinivas <karupal2002@gmail.com>
2021-06-03 07:33:08 +0530

    test code-2
    Signed-off-by: Navin Shrinivas <karupal2002@gmail.com>
    
    Extreamly close to getting this module to work
    only sebfaults eating me up now, As of my debugging so far
    this seg fault seems to be some linux mess!

--- FLOPanalyzer.c
+++ FLOPanalyzer.c
@@ -3,18 +3,11 @@
 #include<stdbool.h>
 #include<pthread.h>
 #include<unistd.h>
-typedef long long int ll;
 
-//structure of the struct so that the function
-//can understand derefrencing
-struct result{
-    int thread_id;
-    ll fadd;
-    ll fsub;
-    ll fdiv;
-    ll fmul;
-};
-void *flopcalculator(void* threadprops){
+typedef unsigned long long int ll;
+
+
+void *faddcalculator(void* thread_ops){
     /*registers are memory locations in cpu,
     here we have used register so as to not loose
     anyperformace due to memory latency*/
@@ -23,95 +16,139 @@ void *flopcalculator(void* threadprops){
     register double v3=0.234e-308; 
     register double v4=0.453e308; 
     register double v5=0.423e-308; 
+    register double v6=0.435e-308; 
+    register double v7=0.45e308; 
+    register double v8=0.234e-308; 
+    register double v9=0.453e308; 
+    register double v10=0.423e-308; 
     //this spam of 64bit double standard floating point numbers
     //is to make sure that compiler doesnt take any advantage of
     //operation specific pipelines that speeds up the process that 
     //is present in CPU pipelines
-
     while(true)
     {
-        double* chc_add=(double*)malloc(sizeof(double));
-        double* chc_sub=(double*)malloc(sizeof(double));
-        double* chc_div=(double*)malloc(sizeof(double));
-        double* chc_mul=(double*)malloc(sizeof(double));
-        if(((struct result*)threadprops)->fadd==0)
-        {
-            *chc_add=v1+v2+v3+v4+v5; //few x86 architechture have a optimised pipline for fadd
-            ((struct result*)threadprops)->fadd+=10;
-        }
-        if(((struct result*)threadprops)->fsub==0)
-        {
-            *chc_sub=v1-v2-v3-v4-v5;
-            ((struct result*)threadprops)->fsub+=10;
-        }
-        if(((struct result*)threadprops)->fmul==0)
-        {
-            *chc_mul=v1*v2*v3*v4*v5; //few x86 architechture have a optimised pipline for fmul
-            ((struct result*)threadprops)->fmul+=10;
-        }
-        if(((struct result*)threadprops)->fdiv==0)
-        {
-            *chc_div=(v1/v2)+(v3/v4)+(v5);
-            ((struct result*)threadprops)->fdiv+=10;
-        }
+        double chc_add=v1+v2+v3+v4+v5+v6+v7+v8+v9+v10; //few x86 architechture have a optimised pipline for fadd
+        (*((ll *)thread_ops))+=10;
     }
 }
+// void *fsubcalculator(){
+//     register double v1=0.435e-308; 
+//     register double v2=0.45e308; 
+//     register double v3=0.234e-308; 
+//     register double v4=0.453e308; 
+//     register double v5=0.423e-308; 
+//     register double v6=0.435e-308; 
+//     register double v7=0.45e308; 
+//     register double v8=0.234e-308; 
+//     register double v9=0.453e308; 
+//     register double v10=0.423e-308; 
+//     while(true)
+//     {
+//         double chc_sub=v1-v2-v3-v4-v5-v6-v7-v8-v9-v10;
+//         c[1]+=10;
+//     }
+// }
+// void *fmulcalculator(){
+//     register double v1=0.435e-308; 
+//     register double v2=0.45e308; 
+//     register double v3=0.234e-308; 
+//     register double v4=0.453e308; 
+//     register double v5=0.423e-308;
+//     register double v6=0.435e-308; 
+//     register double v7=0.45e308; 
+//     register double v8=0.234e-308; 
+//     register double v9=0.453e308; 
+//     register double v10=0.423e-308;  
+//     while(true)
+//     {
+
+//         double chc_mul=v1*v2*v3*v4*v5*v6*v7*v8*v9*v10; //few x86 architechture have a optimised pipline for fmul
+//         c[2]+=10;
+
+//     }
+// }
+// void *fdivcalculator(){
+//     register double v1=0.435e-308; 
+//     register double v2=0.45e308; 
+//     register double v3=0.234e-308; 
+//     register double v4=0.453e308; 
+//     register double v5=0.423e-308;
+//     register double v6=0.435e-308; 
+//     register double v7=0.45e308; 
+//     register double v8=0.234e-308; 
+//     register double v9=0.453e308; 
+//     register double v10=0.423e-308;  
+//     while(true)
+//     {
+      
+        
+//         double chc_div=(v1/v2)+(v3/v4)+(v5/v6)+(v7/v8)+(v9/v10)+0.6;
+//         c[3]+=11;
+//     }
+// }
+
 int main(){
-    int tot;
+    int total_threads;
     printf("Total number of threads to run on :");
-    scanf("%i",&tot);
+    scanf("%i",&total_threads);
     int time;
     printf("Total time for test :");
     scanf("%i",&time);
-    pthread_t threads[tot];
-    struct result results[tot];
-    for(ll i=0;i<tot;i++)
+    printf("Running tests!\n");
+    fflush(stdin);
+    pthread_t threads[total_threads];
+    ll thread_ops[total_threads];
+    for(int i=0;i<total_threads;i++)
     {
-        ((struct result*)results+i)->thread_id=i;
-        ((struct result*)results+i)->fadd=-1;
-        ((struct result*)results+i)->fsub=-1;
-        ((struct result*)results+i)->fmul=-1;
-        ((struct result*)results+i)->fdiv=-1;  
+        thread_ops[i]=0;
     }
-    for(ll i=0;i<tot;i++)
+    printf("Starting fadd module!");
+    for(int i=0;i<total_threads;i++)
     {   
-        printf("Starting fadd module!");
-        ((struct result*)results+i)->fadd=0;
-        pthread_create(&threads[i],NULL,flopcalculator,results+i);
+        pthread_create(&threads[i],NULL,faddcalculator,&thread_ops[i]);
     }
-    sleep(time/4);
-    for(int i=0;i<tot;i++){
+    sleep(10);
+    for(int i=0;i<total_threads;i++){
         pthread_cancel(threads[i]);
     }
-    for(ll i=0;i<tot;i++)
-    {   
-        printf("Starting fsub module!");
-        ((struct result*)results+i)->fsub=0;
-        pthread_create(&threads[i],NULL,flopcalculator,results+i);
-    }
-    sleep(time/4);
-    for(int i=0;i<tot;i++){
-        pthread_cancel(threads[i]);
-    }
-    for(ll i=0;i<tot;i++)
-    {   
-        printf("Starting fmul module!");
-        ((struct result*)results+i)->fmul=0;
-        pthread_create(&threads[i],NULL,flopcalculator,results+i);
-    }
-    sleep(time/4);
-    for(int i=0;i<tot;i++){
-        pthread_cancel(threads[i]);
-    }
-    for(ll i=0;i<tot;i++)
-    {   
-        printf("Starting fdiv module!");
-        ((struct result*)results+i)->fdiv=0;
-        pthread_create(&threads[i],NULL,flopcalculator,results+i);
-    }
-    sleep(time/4);
-    for(int i=0;i<tot;i++){
-        pthread_cancel(threads[i]);
-    }
+    // printf("Starting fsub module!");
+    // for(ll i=0;i<total_threads;i++)
+    // {   
+        
+    //     pthread_create(&threads[i],NULL,fsubcalculator,NULL);
+    //     pthread_join(threads[i],NULL);
+    // }
+    // sleep(time/4);
+    // pthread_exit(NULL);
+    // for(int i=0;i<total_threads;i++){
+    //     pthread_cancel(threads[i]);
+    // }
+    // printf("Starting fmul module!");
+    // for(ll i=0;i<total_threads;i++)
+    // {   
+       
+    //     int a=pthread_create(&threads[i],NULL,fmulcalculator,NULL);
+    //     pthread_join(threads[i],NULL);
+    //     printf("%i",a);
+    // }
+    // sleep(time/4);
+    // for(int i=0;i<total_threads;i++){
+    //     pthread_cancel(threads[i]);
+    // }
+    // printf("Starting fdiv module!");
+    // for(ll i=0;i<total_threads;i++)
+    // {   
+        
+    //     pthread_create(&threads[i],NULL,fdivcalculator,NULL);
+    //     pthread_join(threads[i],NULL);
+    // }
+    // sleep(time/4);
+    // for(int i=0;i<total_threads;i++){
+    //     pthread_cancel(threads[i]);
+    // }
+    for(int i=0;i<total_threads;i++)
+        printf("%llu\n",thread_ops[i]);
+    return 0;
+
 
 }        
