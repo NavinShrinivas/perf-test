fa6a4e1a03695197c27674f07e29138dc1ae60d3
Navin Shrinivas <karupal2002@gmail.com>
2021-06-09 21:44:51 +0530

    fixing multi threading

--- FLOPanalyzer.c
+++ FLOPanalyzer.c
@@ -2,28 +2,31 @@
 #include<pthread.h>
 #include<unistd.h>
 #include<stdbool.h>
+#include<time.h>
 typedef unsigned long long ll;
 
-const double v1=0.3452345543e-2;
-const double v2=0.2342342344e-5;
-const double v3=0.2534534523e-4;
-const double v4=0.6575676788e-5;
-const double v5=0.2345564656e-2;
-const double v6=0.3456573453e-3;
-const double v7=0.9786575454e-5;
-const double v8=0.6754645345e-6;
-const double v9=0.2345435677e-2;
-const double v10=0.6876798956e-4;
 struct opscount{
     ll faddpthread;
     ll fsubpthread;
     ll fmulpthrea;
     ll fdivpthread;
 };
-void *faddtf(void *oppthread)
+/*keeping these two as global variables as thread funtions need these parameters
+during mutithreading control is transffered from the main function hence at luck
+rendering these addresses unacessible by the function leading to seg faults*/ 
+int t;
+ll addarr[256];
+void* faddtf(void *oppthread)
 {
-    double c=0.75665;
-    while(true)
+    register double c=0.75665;
+    register double v1=0.3452345543e-2,v2=0.2342342344e-5,v3=0.2534534523e-4;
+    register double v4=0.6575676788e-5,v5=0.2345564656e-2,v6=0.3456573453e-3;
+    register double v7=0.9786575454e-5,v8=0.6754645345e-6;
+    register double v9=0.2345435677e-2,v10=0.6876798956e-4;
+    clock_t start;
+    start=time(NULL);
+    bool flag=true;
+    while(flag)
     {
         c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;
         c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;
@@ -67,10 +70,15 @@ void *faddtf(void *oppthread)
         c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;c+=v1+v2+v3+v4+v5;c+=v6+v7+v8+v9+v10;
         int op=160*5;
         *((ll *)oppthread)+=op;
+        if(time(NULL)-start>=(t/4))
+        {
+            flag=false;
+        }
     }
+    return NULL;
 }
 int main(){
-    int total_thread,t;
+    int total_thread;
     printf("Enter total number of threads : ");
     scanf("%i",&total_thread);
     printf("Time to run the test : ");
@@ -78,26 +86,40 @@ int main(){
     struct opscount opcount[total_thread];
     pthread_t thread[total_thread];
     //---------------------start of fadd test----------------------------------
-    ll addarr[total_thread];
     for(int i=0;i<total_thread;i++)
     {
+        addarr[i]=0;
         pthread_create(&thread[i],NULL,faddtf,&addarr[i]);
     }
     int optime=t/4;
-    sleep(optime);
+    for(int i=0;i<total_thread;i++)
+	{
+		pthread_join(thread[i],NULL);// waits for the thread fo finish
+	}
     for(int i=0;i<total_thread;i++)
 	{
         opcount[i].faddpthread=addarr[i];
-		pthread_cancel(thread[i]);// kills the threads
-	}
+    }
     ll totalfadd;
+    ll maxfadd=0;
     for(int i=0;i<total_thread;i++)
     {
+        if(maxfadd<opcount[i].faddpthread)
+            maxfadd=opcount[i].faddpthread;
         totalfadd+=opcount[i].faddpthread;
     }
     double totalfaddpsec=totalfadd/optime;
+    double maxfaddpsec=maxfadd/optime;
+    double maxfaddgflop=maxfaddpsec/1000000000;
     double faddgflop=totalfaddpsec/1000000000;
-    printf("FADD : %lf GFlops",faddgflop);
-    fflush(stdout);
-
+    printf("FADD : %lf GFlops [Maximum throughput]\n",faddgflop);
+    printf("FADD : %lf GFlops [Maximim single thread throughput]\n",maxfaddgflop);
 }
+
+
+
+
+
+
+
+
