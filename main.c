b2af79e13df11feb28ff5306d2e3c9625794c157
Navin Shrinivas <karupal2002@gmail.com>
2021-06-10 16:17:55 +0530

    wheee, scriptable stuff.

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
+#include<string.h>
 #include "FLOPanalyzer.h"
-int main(){
-    initfloptest();
+int main(int argc, char* argv[]){
+    if(argc>=2)
+    {
+        printf("Argument entered :%s \n",argv[1]);
+        if(strcmp(argv[1],"help")==0)
+        {
+            printf("List of available module:\n");
+            printf("1.floptest\n");
+        }
+        else if(strcmp(argv[1],"floptest")==0)
+            initfloptest();
+        else{
+            printf("Please enter a valid argument!\n");
+            printf("append help to execution command to see all possible arguments.\n");
+            return 0;
+        }
+        
+    }
+    else
+    {
+        printf("Please enter a argument!\n");
+        printf("append help to execution command to see all possible arguments.\n");
+        return 0;
+    } 
 }
