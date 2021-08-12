fea46d44135dc485adbd73e17eb3bd49195cf995
NavinShrinivas <karupal2002@gmail.com>
2021-08-12 04:47:14 +0000

    Few updates to installer
    Signed-off-by: NavinShrinivas <karupal2002@gmail.com>
    
    updates to pthreads , pthreads is now not included from posix , I am
    guessing this will help during transistion to windows

--- install.sh
+++ install.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+#If build is failing we are adding an extra optin that cleans the bulidenv to start from scratch
 
-echo "==========PERF-TOOL=========="
-echo "This is a command line installer for our tool."
-echo "This script will install the following packages and libaries"
-echo "1.Vcpkg [Package]"
-echo "2.Curl [Libaries]"
-echo "3.Pthreads [Library]" echo "" echo ""
-read -p $'\n\nContinue with installation [Y/n] :' option
-if [[ "$option" == "n" ]]; then
-    echo "See you later , bye bye :)"
+if [[ "$1" == "--reset" ]]; then
+    sudo rm -r ./buildenv
+elif [[ "$1" == "--install" ]]; then
+    echo "==========PERF-TOOL=========="
+    echo "This is a command line installer for our tool."
+    echo "This script will install the following packages and libaries"
+    echo "1.Vcpkg [Package]"
+    echo "2.Curl [Libaries]"
+    echo "3.Pthreads [Library]" echo "" echo ""
+    read -p $'\n\nContinue with installation [Y/n] :' option
+    if [[ "$option" == "n" ]]; then
+        echo "See you later , bye bye :)"
+    else
+        if [[ "$2" == "--apt" ]]; then
+            sudo apt-get install build-essential tar zip unzip cmake
+        else
+            sudo pacman -S base-devel tar zip unzip cmake
+        fi
+        echo "Starting install process"
+        mkdir buildenv
+        git clone https://github.com/Microsoft/vcpkg.git ./buildenv/vcpkg
+        # ./buildenv/vcpkg/bootstrap-vcpkg.sh
+        cd ./buildenv/vcpkg/
+        ./vcpkg install curl
+        ./vcpkg install pthreads
+        # vcpkg install pthreads "not needed for linux , i.e a shell script"
+        cd .. && cd ..
+        cmake . -DCMAKE_TOOLCHAIN_FILE=./buildenv/vcpkg/scripts/buildsystems/vcpkg.cmake
+        make
+    fi
 else
-    echo "Starting install process"
-    mkdir buildenv
-    git clone https://github.com/Microsoft/vcpkg.git ./buildenv/vcpkg
-    ./buildenv/vcpkg/bootstrap-vcpkg.sh
-    cd ./buildenv/vcpkg/
-    ./vcpkg install curl
-    # vcpkg install pthreads "not needed for linux , i.e a shell script"
-    cd .. && cd ..
-    cmake . -DCMAKE_TOOLCHAIN_FILE=./buildenv/vcpkg/scripts/buildsystems/vcpkg.cmake
-    make
+    echo "Please enter a valid argument!"
 fi
